6e103282d12fcbc22b126b6f1f63f9babc41e523
liuli <liuli521>
2017-11-06 10:21:40 +0800

    修改账号绑定问题

--- common/js/common.js
+++ common/js/common.js
@@ -5,10 +5,10 @@ $(function(){
 	util.serviceName = "/authority_service";
     util.webName = "/authority_web";
 
-	//util.baseUrl = "http://fx.dev.galaxyinternet.com" + util.serviceName;			//dev
+	util.baseUrl = "http://fx.dev.galaxyinternet.com" + util.serviceName;			//dev
     //util.baseUrl = "http://fx.galaxyinternet.com" + util.serviceName;				//online
     //util.baseUrl = "http://fx.local.galaxyinternet.com" + util.serviceName;		//local
-	util.baseUrl = "http://fx.qa.galaxyinternet.com" + util.serviceName;			//qa
+	//util.baseUrl = "http://fx.qa.galaxyinternet.com" + util.serviceName;			//qa
 	//util.baseUrl = "http://fx.rc.galaxyinternet.com" + util.serviceName;			//rc
 	
 	/**
