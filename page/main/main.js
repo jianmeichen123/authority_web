76d6b8f13f528f343e6f11e75ab85289062e9af1
liuli <liuli521>
2018-01-29 13:29:27 +0800

    默认部门

--- page/main/main.js
+++ page/main/main.js
@@ -4,7 +4,7 @@
 $(function(){
 	//设置默认菜单-账号
     $("#menu_role").css("color","#199ed7");
-    $("#content").attr("src",$.util.webName + "/page/role/role.html");  //?sessionId=" + $.util.getSessionId()
+    $("#content").attr("src",$.util.webName + "/page/depart/depart.html");  //?sessionId=" + $.util.getSessionId()
 
     //设置登录人姓名
     $("#span_user_name").html($.cookie("userName"));
